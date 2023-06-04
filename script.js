6f1f07402ae42e838986ce45854f67f2b8c3d5cd
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-04 13:17:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let st=document.getElementsByTagName("strong");
 function highlight() {
     //Write your code here
-
+	for(let i=0;i<st.length;i++){
+		st[i].style.color="green";
+	}
 
 }
-
-
 function return_normal() {
     //Write your code here
-
+for(let i=0;i<st.length;i++){
+		st[i].style.color="black";
+	}
     
 }
